3b4ea192c44a2c44e1e9818348f572e63bc89cb7
Patrick Gnewikow <patrick@gnewikow.net>
2023-01-16 13:45:12 +0100

    - forgot composer - fixed asset typo

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/admin', name: 'app_admin')]
+#[Route('/admin')]
 class AdminController extends AbstractController
 {
     #[Route('/', name: 'app_admin')]
