a31e8c16dc97b24c681c55913bff262358c47106
Patrick Gnewikow <patrick@gnewikow.net>
2023-03-03 14:51:09 +0100

    - merge aftermath

---
+++ src/Twig/Extension/MerryweatherExtension.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Twig\Extension;
+
+use App\Twig\Runtime\MerryweatherExtensionRuntime;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class MerryweatherExtension extends AbstractExtension
+{
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('canBook', [MerryweatherExtensionRuntime::class, 'canBook']),
+            new TwigFunction('canCancel', [MerryweatherExtensionRuntime::class, 'canCancel']),
+            new TwigFunction('slotCost', [MerryweatherExtensionRuntime::class, 'slotCost']),
+            new TwigFunction('bootstrapClassForLog', [MerryweatherExtensionRuntime::class, 'bootstrapClassForLog']),
+        ];
+    }
+}
