0d4d477b42c58e06adc9404da6a0db99924da070
Patrick Gnewikow <patrick@gnewikow.net>
2023-04-10 22:33:17 +0200

    - manifest

---
+++ src/Controller/ManifestController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ManifestController extends AbstractController
+{
+    #[Route('/manifest.{_locale}.json', name: 'app_manifest_loc')]
+    #[Route('/manifest.json', name: 'app_manifest')]
+    public function index(): Response
+    {
+
+        return $this->render('manifest.json.twig', [
+        ],new JsonResponse());
+    }
+}
