90deb3db25e84603d0dea2629026ba78b34973ad
Patrick Gnewikow <patrick@gnewikow.net>
2023-05-12 13:10:32 +0200

    - wrong table

--- migrations/Version20230510130822.php
+++ migrations/Version20230510130822.php
@@ -62,8 +62,7 @@ final class Version20230510130822 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE slot DROP FOREIGN KEY FK_AC0E20676EB6DDB5');
-        $this->addSql('ALTER TABLE slot DROP FOREIGN KEY FK_AC0E2067A76ED395');
+
         $this->addSql('ALTER TABLE app_config CHANGE id id INT AUTO_INCREMENT NOT NULL');
 
         $this->addSql('ALTER TABLE slot CHANGE id id INT AUTO_INCREMENT NOT NULL, CHANGE distribution_id distribution_id INT NOT NULL, CHANGE user_id user_id INT DEFAULT NULL');
@@ -108,13 +107,22 @@ final class Version20230510130822 extends AbstractMigration
 
     public function preDown(Schema $schema): void
     {
+        $this->connection->executeStatement('ALTER TABLE slot DROP FOREIGN KEY FK_AC0E20676EB6DDB5');
+        $this->connection->executeStatement('ALTER TABLE slot DROP FOREIGN KEY FK_AC0E2067A76ED395');
         foreach (['app_config', 'crontab'] as $table) {
             $this->fromUUID($table);
+        }
+        $newId = 0;
+        $distIds = $this->connection->executeQuery('SELECT id FROM distribution')->fetchAllAssociative();
+        foreach ($distIds as $distId) {
+            $newId++;
+            $this->connection->executeStatement('UPDATE distribution SET id = "' . $newId . '" WHERE id= "' . $distId['id'] . '"');
+            $this->connection->executeStatement('UPDATE slot SET distribution_id = "' . $newId . '" WHERE distribution_id= "' . $distId['id'] . '"');
         }
         $userIds = $this->connection->executeQuery('SELECT id FROM user')->fetchAllAssociative();
         $newId = 0;
         foreach ($userIds as $userId) {
-            $slotIds = $this->connection->executeQuery('SELECT id FROM slot WHERE user_id = ' . $userId['id'])->fetchAllAssociative();
+            $slotIds = $this->connection->executeQuery('SELECT id FROM slot WHERE user_id = "' . $userId['id'] . '"')->fetchAllAssociative();
             $newId++;
             $this->connection->executeStatement('UPDATE slot SET user_id = NULL WHERE user_id= "' . $userId['id'] . '"');
             $this->connection->executeStatement('UPDATE user SET id = "' . $newId . '" WHERE id= "' . $userId['id'] . '"');
@@ -128,13 +136,7 @@ final class Version20230510130822 extends AbstractMigration
             $newId++;
             $this->connection->executeStatement('UPDATE slot SET id = "' . $newId . '" WHERE id= "' . $slotId['id'] . '"');
         }
-        $distIds = $this->connection->executeQuery('SELECT id FROM distribution')->fetchAllAssociative();
-        $newId = 0;
-        foreach ($distIds as $distId) {
-            $newId++;
-            $this->connection->executeStatement('UPDATE distribution SET id = "' . $newId . '" WHERE id= "' . $distId['id'] . '"');
-            $this->connection->executeStatement('UPDATE slot SET distribution_id = "' . $newId . '" WHERE distribution_id= "' . $distId['id'] . '"');
-        }
+
     }
 
     private function genUUID(int $offset = 0): string
